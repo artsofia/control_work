9cb68afb7db6c13c9307ac44c456fbed2dfabf15
Sophia <artsophia1621@gmail.com>
2023-09-04 21:02:23 +0400

    Add a program that solves the task

---
+++ control_task/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel;
+
+namespace task;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string InputStr(string text)
+            {
+                Console.Write(text);
+                return Console.ReadLine();
+            }
+
+
+        int InputInt(string text)
+            {
+                Console.Write(text);
+                return Convert.ToInt32(Console.ReadLine());
+            }
+
+
+        string[] FillArray()
+            { 
+                int sizeArray = InputInt("Введите колличество элементов в массиве: ");
+                string[] array = new string[sizeArray];
+                
+                for (int i = 0; i < sizeArray; i++)
+                {
+                    array[i] = InputStr("Введите элемент массива: ");
+                }
+
+                return array;
+            }
+
+
+        void NewArray(string[] array)
+        {     
+            if (array.Length <= 3)
+            {
+                Console.WriteLine("[]");
+            }
+            else
+            {
+                Console.Write("[");
+
+                for (int i = 3; i < array.Length; i++)
+                    {
+                        Console.Write(array[i] + ", ");
+                    }
+
+                Console.Write("\b\b");
+                Console.Write("]");
+            }
+        }
+
+        Console.Clear();
+        string[] array = FillArray();
+        NewArray(array);
+    }   
+}
